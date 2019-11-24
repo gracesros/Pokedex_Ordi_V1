d932f599054c14fef31a8c78502291f9f4cfce02
grace <gracesros@hotmail.com>
2019-11-23 21:50:13 -0600

    Initial commit

---
+++ app/src/main/java/org/grace/pokedex/PokemonTypeActivity.java
@@ -0,0 +1,96 @@
+package org.grace.pokedex;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import org.grace.pokedex.adapters.DamageRelationAdapter;
+import org.grace.pokedex.adapters.PokemonAdapter;
+import org.grace.pokedex.data.Pokemon;
+import org.grace.pokedex.data.PokemonType;
+import org.grace.pokedex.interfaces.AsyncTaskHandler;
+import org.grace.pokedex.utils.TypeUtils;
+
+
+public class PokemonTypeActivity extends AppCompatActivity implements AsyncTaskHandler, PokemonAdapter.ItemClickListener {
+
+    TextView name;
+    RecyclerView damageRelations;
+    RecyclerView pokemons;
+    PokemonAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pokemon_type);
+
+        name = findViewById(R.id.type_name);
+        damageRelations = findViewById(R.id.type_damage_relations);
+        pokemons = findViewById(R.id.type_pokemons);
+
+        String type = getIntent().getStringExtra("TYPE");
+        String url = "https://pokeapi.co/api/v2/type/" + type;
+
+        TypeUtils pokemonTypeAsyncTask = new TypeUtils();
+        pokemonTypeAsyncTask.handler = this;
+        pokemonTypeAsyncTask.execute(url);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_top, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_favorites:
+                Intent intent = new Intent(this, FavoritesActivity.class);
+                startActivity(intent);
+                return true;
+            case android.R.id.home:
+                // app icon in action bar clicked; go home
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onTaskEnd(Object result) {
+        PokemonType pokemonType = (PokemonType) result;
+
+        name.setText(pokemonType.getName());
+
+        damageRelations.setLayoutManager(new LinearLayoutManager(this));
+        damageRelations.setAdapter(new DamageRelationAdapter(this, pokemonType));
+
+        pokemons.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new PokemonAdapter(this, pokemonType.getPokemons());
+        adapter.setClickListener(this);
+        pokemons.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Pokemon pokemon = adapter.getPokemon(position);
+
+        Intent intent = new Intent(this, PokemonDetailsActivity.class);
+        intent.putExtra("URL", pokemon.getUrl());
+        startActivity(intent);
+    }
+}
